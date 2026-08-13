b68f3eb046e25f866308a52699f2632d84569818
Erick Navarro <erick@navarro.io>
2026-08-13 00:58:01 -0600

    chore(hitsu): update to v0.3.0
    Remove the xattr workaround now that the app is notarized.

--- Casks/hitsu.rb
+++ Casks/hitsu.rb
@@ -1,8 +1,8 @@
 cask "hitsu" do
   arch arm: "aarch64"
 
-  version "0.1.0"
-  sha256 arm:   "8b2bef6b7d06ea771b91722de4f6f34b23dece1a6cc53e9f0935f11c025ffca9"
+  version "0.3.0"
+  sha256 arm:   "cb79bef0d215a19222fe08c7ddf713e63f1610a76c630e29230e9524d3a493b2"
 
   url "https://github.com/ruaylabs/hitsu/releases/download/v#{version}/Hitsu_#{version}_#{arch}.dmg"
   name "Hitsu"
@@ -18,13 +18,6 @@ cask "hitsu" do
   depends_on macos: :sonoma
 
   app "Hitsu.app"
-
-  # Not notarized by Apple — strip quarantine so Gatekeeper doesn't block first launch
-  postflight do
-    system_command "/usr/bin/xattr",
-                    args: ["-cr", "#{appdir}/Hitsu.app"],
-                    sudo: false
-  end
 
   zap trash: [
     "~/Library/Application Support/com.ruaylabs.hitsu",
